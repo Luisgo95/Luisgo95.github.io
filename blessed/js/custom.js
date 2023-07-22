92eec18ac070d80d4dadc4a9db43d09fe4c517b7
Luisgo95 <lgomezm8@miumg.edu.gt>
2023-07-22 13:45:34 -0600

    plantilla entrega 1

--- blessed/js/custom.js
+++ blessed/js/custom.js
@@ -263,7 +263,7 @@ $(function () {
 		$this.countdown(finalDate, function (event) {
 			var $this = $(this).html(event.strftime(''
 				+ '<div class="time-bar"><span class="time-box">%w</span> <span class="line-b">weeks</span></div> '
-				+ '<div class="time-bar"><span class="time-box">%d</span> <span class="line-b">days</span></div> '
+				+ '<div class="time-bar"><span class="time-box">%d</span> <span class="line-b">dìas</span></div> '
 				+ '<div class="time-bar"><span class="time-box">%H</span> <span class="line-b">hr</span></div> '
 				+ '<div class="time-bar"><span class="time-box">%M</span> <span class="line-b">min</span></div> '
 				+ '<div class="time-bar"><span class="time-box">%S</span> <span class="line-b">sec</span></div>'));
